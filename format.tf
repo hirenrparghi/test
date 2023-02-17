3c24297541c72afbb414cf810e036d01b9b67803
hirenrparghi <71499849+hirenrparghi@users.noreply.github.com>
2023-02-17 13:21:04 +0530

    Create format.tf

---
+++ format.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region     = "ap-south-1"
+  secret_key = "qTsiB6aQtV0z+Q6HThv6IZ1OLhBm8BykppJQr1qu"
+  access_key = "AKIAVPO6L223HQK65VMC"
+}
+
+resource "aws_instance" "ec2-1" {
+    ami = "ami-06984ea821ac0a879"
+    instance_type = "t2.micro"
+ 
+tags = {
+  "Name" = "Latest"
+}
+ 
+}
+
+
+#Configure the Microsoft Azure Provider
+# provider "azurerm" {
+
+# }
+
+
+# terraform {
+#   required_providers {
+#     digitalocean = {
+#       source = "digitalocean/digitalocean"
+#       version = "2.26.0"
+#     }
+#   }
+# }
