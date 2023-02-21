d35ca100245ba0f8dd03b5331ebb6080218cd3e0
hiren <hirenparghi90@gmail.com>
2023-02-21 10:51:16 +0530

    second commit

--- format.tf
+++ format.tf
@@ -1,8 +1,8 @@
-provider "aws" {
-  region     = "ap-south-1"
-  secret_key = "qTsiB6aQtV0z+Q6HThv6IZ1OLhBm8BykppJQr1qu"
-  access_key = "AKIAVPO6L223HQK65VMC"
-}
+# provider "aws" {
+#   region     = "ap-south-1"
+#   secret_key = "qTsiB6aQtV0z+Q6HThv6IZ1OLhBm8BykppJQr1qu"
+#   access_key = "AKIAVPO6L223HQK65VMC"
+# }
 
 resource "aws_instance" "ec2-1" {
     ami = "ami-06984ea821ac0a879"
